0f6a20c3bb6b0fffabca986c1e3bc02b40914399
Yu [  ] <70889316+Yu-225@users.noreply.github.com>
2022-02-10 23:02:12 +0200

    Update Timer.py

--- Timer.py
+++ Timer.py
@@ -7,7 +7,7 @@ class TimerMod(loader.Module):
     """Timer""" 
     strings = {'name': 'Timer'} 
     
-    async def timercmd(self, event):
+    async def timercmd(self, message):
         """Timer sec"""
         time = utils.get_args_raw(message)
         await event.edit(time + ' sec')
