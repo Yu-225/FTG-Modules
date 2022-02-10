2353dae5a242c0627ef5ce78e3ebfaf3eeb6e260
Yu [  ] <70889316+Yu-225@users.noreply.github.com>
2022-02-10 23:29:27 +0200

    Add files via upload

--- Timer.py
+++ Timer.py
@@ -10,4 +10,12 @@ class TimerMod(loader.Module):
     async def timercmd(self, event):
         """Timer sec"""
         time = utils.get_args_raw(event)
-        await event.edit(time + ' sec aaa')
+        
+        try: int(time)
+        except: await event.edit('Error: Invalid You і твоя команда')
+
+        for i in range(time):
+            await event.edit(time + ' sec')
+            sleep(1)
+            time-=1
+        await event.edit('ypa )')
